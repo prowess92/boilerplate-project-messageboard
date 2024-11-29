9181fa5ac5cf90114c0c54d77e7f5b0a71146bc1
John Mwayiwanga Mtingwi <mwmtingwi5@gmail.com>
2024-11-29 12:29:54 +0000

    Added tests

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
 require('dotenv').config();
-const express     = require('express');
-const helmet      = require('helmet');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
@@ -18,12 +18,16 @@ app.use(helmet({
       styleSrc: ["'self'"],
       scriptSrc: ["'self'"],
       connectSrc: ["'self'"]
-    }
+    },
+    loose: true  
+  },
+  referrerPolicy: {
+    policy: 'strict-origin-when-cross-origin'
   }
 }))
 
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -51,7 +55,7 @@ fccTestingRoutes(app);
 apiRoutes(app);
 
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -60,12 +64,12 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
